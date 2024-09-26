c7b4aafb3990335c74bacbbf3b2a1017070745f5
amitcjc <amitcjcpune@gmail.com>
2024-09-26 08:45:50 +0530

    by cjc

--- tb-27Git/src/com/Test.java
+++ tb-27Git/src/com/Test.java
@@ -2,6 +2,14 @@ package com;
 
 public class Test 
 {
+	public void m1()
+	{
+		System.out.println("This is M1");
+	}
+	
+	
+	
+	
 public static void main(String[] args) 
 {
 System.out.println("This is Main Method");	
